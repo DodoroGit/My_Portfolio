095d11cb764d2a26cd302facca481928501fb958
Rick-Wj <dokebi871218@gmail.com>
2025-02-28 19:05:12 +0800

    2025/2/28: aws ec2 testing

--- backend/main.go
+++ backend/main.go
@@ -32,7 +32,7 @@ func GetUsers(c *gin.Context) {
 }
 
 func GetProjects(c *gin.Context) {
-	c.File("./home/ec2-user/My_Portfolio/frontend/index.html")
+	c.File("/home/ec2-user/My_Portfolio/frontend/index.html")
 }
 
 func main() {
