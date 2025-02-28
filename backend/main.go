21f7f25648c63e218aebf86d24b7dddd4502f42b
Rick-Wj <dokebi871218@gmail.com>
2025-02-28 18:53:40 +0800

    2025/2/28: aws ec2 testing

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq" // 🚀 這行很重要，確保 PostgreSQL 驅動被加載
+	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
@@ -32,13 +32,7 @@ func GetUsers(c *gin.Context) {
 }
 
 func GetProjects(c *gin.Context) {
-	projects := []map[string]string{
-		{"name": "個人作品集", "description": "展示我的個人專案", "url": "projects.html"},
-		{"name": "E-commerce 平台", "description": "一個簡單的線上購物網站", "url": "#"},
-		{"name": "部落格系統", "description": "基於 Gin 框架開發的部落格平台", "url": "#"},
-	}
-
-	c.JSON(200, gin.H{"projects": projects})
+	c.File("./frontend/index.html")
 }
 
 func main() {
@@ -46,11 +40,12 @@ func main() {
 	InitPostgres()
 
 	r := gin.Default()
+	r.Static("/static", "./frontend")
+
 	r.GET("/users", GetUsers)
 	r.GET("/projects", GetProjects)
 
 	if err := r.Run("0.0.0.0:8080"); err != nil {
 		log.Fatal("Unable to start server: ", err)
 	}
-
 }
