50754ac7d8b755750efee257859665408712d19f
Rick-Wj <dokebi871218@gmail.com>
2025-03-07 01:33:08 +0800

    2025/2/15: organize my previous projects

--- backend/routes/web_routes.go
+++ backend/routes/web_routes.go
@@ -3,7 +3,6 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func WebRoutes(r *gin.Engine) {
-	r.Static("/assets", "/home/ec2-user/My_Portfolio/frontend/assets")
 	r.LoadHTMLGlob("/home/ec2-user/My_Portfolio/frontend/*.html")
 
 	r.GET("/index", func(c *gin.Context) { c.HTML(200, "index.html", nil) })
@@ -12,6 +11,6 @@ func WebRoutes(r *gin.Engine) {
 	r.GET("/skills", func(c *gin.Context) { c.HTML(200, "skills.html", nil) })
 	r.GET("/contact", func(c *gin.Context) { c.HTML(200, "contact.html", nil) })
 
-	r.GET("/login", func(c *gin.Context) { c.HTML(200, "auth.html", nil) })
+	r.GET("/auth", func(c *gin.Context) { c.HTML(200, "auth.html", nil) })
 	r.GET("/dashboard", func(c *gin.Context) { c.HTML(200, "dashboard.html", nil) })
 }
