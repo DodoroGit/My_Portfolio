dfd2004f84bb193cbc5c75e1aff583c7ebcb70c1
Rick-Wj <dokebi871218@gmail.com>
2025-05-21 22:31:57 +0800

    feat(2025/5/21): add export excel for expense function

--- backend/routes/expense_routes.go
+++ backend/routes/expense_routes.go
@@ -13,6 +13,6 @@ func ExpenseRoutes(r *gin.Engine) {
 		expense.POST("/", handlers.CreateExpense)
 		expense.GET("/", handlers.GetExpenses)
 		// expense.POST("/upload", handlers.UploadExcel) // 後續擴充
-		// expense.GET("/export", handlers.ExportExcel)   // 後續擴充
+		expense.GET("/export", handlers.ExportExcel) // 後續擴充
 	}
 }
